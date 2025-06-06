5d8dbc176c31f830845a64ac9026303a67fca37f
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2025-06-06 13:28:45 +0200

    Arreglado fondo de estrellas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-      {/* <StarsCanvas /> */}
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Tech />
-        <Experience />
-        <Works />
-        {/* <Feedbacks /> */}
-        <div className='relative z-0'>
-          <Contact />
+        <StarsCanvas />
+        <div className='main-content'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Tech />
+          <Experience />
+          <Works />
+          {/* <Feedbacks /> */}
+          <div className='relative z-0'>
+            <Contact />
+          </div>
         </div>
       </div>
     </BrowserRouter>
