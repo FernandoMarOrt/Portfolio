7605f80a49cafa0d17c4b0f0a92f31ea71135623
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2025-07-24 20:15:59 +0200

    refactor: Implement lazy loading for components in App to enhance performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import React, { Suspense, lazy } from "react";
+import { Hero, Navbar } from "./components";
 import { ThemeProvider, useTheme } from "./contexts/ThemeContext";
 import ScrollProgress from "./components/ScrollProgress";
+
+const About = lazy(() => import("./components/About"));
+const Tech = lazy(() => import("./components/Tech"));
+const Experience = lazy(() => import("./components/Experience"));
+const Works = lazy(() => import("./components/Works"));
+const Contact = lazy(() => import("./components/Contact"));
+const StarsCanvas = lazy(() => import("./components/canvas/Stars"));
 
 const AppContent = () => {
   const { showStars } = useTheme();
@@ -11,20 +19,23 @@ const AppContent = () => {
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <ScrollProgress />
-        {showStars && <StarsCanvas />}
-        
+        <Suspense fallback={null}>
+          {showStars && <StarsCanvas />}
+        </Suspense>
         <div className='main-content'>
           <div className='relative'>
             <Navbar />
             <Hero />
           </div>
-          <About />
-          <Tech />
-          <Experience />
-          <Works />
-          <div className='relative z-0'>
-            <Contact />
-          </div>
+          <Suspense fallback={null}>
+            <About />
+            <Tech />
+            <Experience />
+            <Works />
+            <div className='relative z-0'>
+              <Contact />
+            </div>
+          </Suspense>
         </div>
       </div>
     </BrowserRouter>
